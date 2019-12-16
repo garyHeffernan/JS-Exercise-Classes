06b2c7cf05872e1e7dddf73083ecbe92e4da25ee
garyHeffernan <gary.heffernan@me.com>
2019-12-15 20:31:34 -0800

    Commit 44 passing

--- index.js
+++ index.js
@@ -143,10 +143,10 @@ class Instructor extends Lambdasian {
     this.catchPhrase = obj.catchPhrase;
   }
   demo(subject) {
-
+    return `Today we are learning about ${subject}`;
   }
   grade(student, subject){
-
+    return `${student.name} receives a perfect score on ${subject}`
   }
 }
 
@@ -165,8 +165,22 @@ class Instructor extends Lambdasian {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
-
+class Student extends Lambdasian {
+  constructor(obj){
+    super(obj);
+    this.previousBackground = obj.previousBackground;
+    this.className = obj.className;
+    this.favSubjects = obj.favSubjects;
+  }
+  listSubjects(){
+    return this.favSubjects.toString();
+  }
+  PRAssignment(subject){
+    return `${this.name} has submitted a PR for ${subject}`
+  }
+  sprintChallenge(subject){
+    return `${this.name} has begun sprint challenge on ${subject}`
+  }
 }
 
 /*
